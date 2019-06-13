21283769d07f6832f08b0265463e8cbd52d5e161
SangHyuk Henry Jung <onlymytho@gmail.com>
2019-06-14 00:23:47 +0900

    Updates

---
+++ precache-manifest.76868d7a96aeaa23dcbecfdc5cce0762.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f309c7e1a2e10a63f7b458ebd6d685ba",
+    "url": "/curious-web/movieql/index.html"
+  },
+  {
+    "revision": "35a1311d44bf2205fe19",
+    "url": "/curious-web/movieql/static/css/main.e1bde1d4.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/curious-web/movieql/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "35a1311d44bf2205fe19",
+    "url": "/curious-web/movieql/static/js/main.c1d10117.chunk.js"
+  },
+  {
+    "revision": "622382dff8d8918d6bbd",
+    "url": "/curious-web/movieql/static/js/runtime~main.3cbd0b79.js"
+  }
+]);
